e3346bd4fb8c6ccb0e0c958bb0cd6127e0d63fc6
Bert Constantin <bert.constantin@gmx.de>
2010-01-25 16:47:28 +0100

    move test db to /var/tmp/

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/tmp/django-polymorphic-test-db.sqlite3'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/var/tmp/django-polymorphic-test-db.sqlite3'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 
