74a21efb176050dcb877551ad68c05e8c939e65d
Chris Glass <christopher.glass@divio.ch>
2011-04-27 11:10:06 +0200

    Changed setup.py: Added maintainer field (me), and classifiers

--- setup.py
+++ setup.py
@@ -6,6 +6,17 @@ setup(
     description = 'Seamless Polymorphic Inheritance for Django Models',
     author = 'Bert Constantin',
     author_email = 'bert.constantin@gmx.de',
+    maintainer = 'Christopher Glass',
+    maintainer_email = 'tribaal@gmail.com'
     url = 'http://bserve.webhop.org/wiki/django_polymorphic',
     packages = [ 'polymorphic' ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
