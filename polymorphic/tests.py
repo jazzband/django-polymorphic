2af08e0f39556dab414e3b4dd3b1facce1390bc0
Jedediah Smith <jedediah@silencegreys.com>
2012-11-14 17:24:52 -0500

    Support proxy models (still requires one query per proxied model, not optimal)

--- polymorphic/tests.py
+++ polymorphic/tests.py
@@ -633,3 +633,37 @@ __test__ = {"doctest": """
 
 """}
 
+
+class MonkeyPatchTests(TestCase):
+
+    def test_content_types_for_proxy_models_patch(self):
+        from django.db.models import Model
+        from django.contrib.contenttypes.models import ContentType
+
+        class Base(Model):
+            pass
+
+        class Proxy(Base):
+            class Meta:
+                proxy = True
+
+        ct = ContentType.objects.get_for_model(Proxy, for_concrete_model=False)
+        self.assertEqual(Proxy, ct.model_class())
+
+    def test_content_types_for_proxy_models_patch_still_required(self):
+        """
+        If this test fails then our monkey patch of ContentTypeManager.get_for_model
+        is no longer required and should be removed
+        """
+        from django.db.models import Model
+        from django.contrib.contenttypes.models import ContentType
+
+        class MyModel(Model):
+            pass
+
+        self.assertRaisesMessage(
+            TypeError,
+            "get_for_model() got an unexpected keyword argument 'for_concrete_model'",
+            ContentType.objects.get_for_model__original,
+            MyModel, for_concrete_model=False
+        )
