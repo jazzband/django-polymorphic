74389bb23dcf7459322ebc0341d494527fdaaae2
floppya <floppya@gmail.com>
2013-03-20 17:38:03 -0600

    Update for proxy models and Django 1.5
    Only monkeypatch when using Django < 1.5
    Updated test

--- polymorphic/__init__.py
+++ polymorphic/__init__.py
@@ -6,6 +6,7 @@ Copyright:
 This code and affiliated files are (C) by Bert Constantin and individual contributors.
 Please see LICENSE and AUTHORS for more information.
 """
+import django
 from polymorphic_model import PolymorphicModel
 from manager import PolymorphicManager
 from query import PolymorphicQuerySet
@@ -25,37 +26,34 @@ def get_version():
         version += ' %s' % VERSION[3]
     return version
 
-from django.contrib.contenttypes.models import ContentTypeManager
-from django.utils.encoding import smart_unicode
 
-
-# Monkey-patch Django to allow ContentTypes for proxy models. This is compatible with an
-# upcoming change in Django 1.5 and should be removed when we upgrade. There is a test
-# in MonkeyPatchTests that checks for this.
-# https://code.djangoproject.com/ticket/18399
-
-def get_for_model(self, model, for_concrete_model=True):
+# Monkey-patch Django < 1.5 to allow ContentTypes for proxy models.
+if django.VERSION[:2] < (1, 5): 
+    from django.contrib.contenttypes.models import ContentTypeManager
     from django.utils.encoding import smart_unicode
 
-    if for_concrete_model:
-        model = model._meta.concrete_model
-    elif model._deferred:
-        model = model._meta.proxy_for_model
+    def get_for_model(self, model, for_concrete_model=True):
+        from django.utils.encoding import smart_unicode
 
-    opts = model._meta
+        if for_concrete_model:
+            model = model._meta.concrete_model
+        elif model._deferred:
+            model = model._meta.proxy_for_model
 
-    try:
-        ct = self._get_from_cache(opts)
-    except KeyError:
-        ct, created = self.get_or_create(
-            app_label = opts.app_label,
-            model = opts.object_name.lower(),
-            defaults = {'name': smart_unicode(opts.verbose_name_raw)},
-        )
-        self._add_to_cache(self.db, ct)
+        opts = model._meta
 
-    return ct
+        try:
+            ct = self._get_from_cache(opts)
+        except KeyError:
+            ct, created = self.get_or_create(
+                app_label = opts.app_label,
+                model = opts.object_name.lower(),
+                defaults = {'name': smart_unicode(opts.verbose_name_raw)},
+            )
+            self._add_to_cache(self.db, ct)
 
-ContentTypeManager.get_for_model__original = ContentTypeManager.get_for_model
-ContentTypeManager.get_for_model = get_for_model
+        return ct
+
+    ContentTypeManager.get_for_model__original = ContentTypeManager.get_for_model
+    ContentTypeManager.get_for_model = get_for_model
 
