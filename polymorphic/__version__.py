9b9838116df4230166bf78ef4be918c2b6b5e418
Chris Glass <tribaal@gmail.com>
2013-04-10 14:15:05 +0200

    Bumped version to 0.4.2
    Previous verison had a broken setup.py

---
+++ polymorphic/__version__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+See PEP 386 (https://www.python.org/dev/peps/pep-0386/)
+
+Release logic:
+    1. Remove "dev#" from current (this file, now).
+    2. git commit
+    3. git tag <version>
+    4. push to pypi + push --tags to github
+    5. bump the version, append ".dev0"
+    6. git commit
+    7. push to github
+"""
+__version__ = "0.4.2"
+
