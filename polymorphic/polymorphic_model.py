578aa8f1dd14027f2b37651f342b9f905f741485
hottwaj <jonathan.a.clarke@gmail.com>
2014-02-19 11:00:19 +0000

    removed pdb import which I used for testing...

--- polymorphic/polymorphic_model.py
+++ polymorphic/polymorphic_model.py
@@ -22,7 +22,6 @@ from django.utils import six
 from .base import PolymorphicModelBase
 from .manager import PolymorphicManager
 from .query_translate import translate_polymorphic_Q_object
-import pdb
 
 ###################################################################################
 ### PolymorphicModel
