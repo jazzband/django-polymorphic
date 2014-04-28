f85ebdd9e6ed916149d8f3f739cbfd5d1bf34be5
John Furr <jfurr@Johns-MacBook-Pro.local>
2014-04-28 11:32:30 -0400

    Fix get_real_instance_class for proxy models

--- polymorphic/polymorphic_model.py
+++ polymorphic/polymorphic_model.py
@@ -110,7 +110,9 @@ class PolymorphicModel(six.with_metaclass(PolymorphicModelBase, models.Model)):
 
         # Protect against bad imports (dumpdata without --natural) or other
         # issues missing with the ContentType models.
-        if model is not None and not issubclass(model, self.__class__):
+        if model is not None \
+        and not issubclass(model, self.__class__) \
+        and not issubclass(model, self.__class__._meta.proxy_for_model):
             raise RuntimeError("ContentType {0} for {1} #{2} does not point to a subclass!".format(
                 self.polymorphic_ctype_id, model, self.pk,
             ))
