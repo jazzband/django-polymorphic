9d68dee7dd05b6f73231eec85963ca11b676fdb5
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-30 11:35:19 +0200

    __version__.py: reference PEP 440 instead.

--- polymorphic/__version__.py
+++ polymorphic/__version__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-See PEP 386 (https://www.python.org/dev/peps/pep-0386/)
-
-Release logic:
-    1. Remove "dev#" from current (this file, now AND setup.py!).
-    2. git commit
-    3. git tag <version>
-    4. push to pypi + push --tags to github
-    5. bump the version, append ".dev0"
-    6. git commit
-    7. push to github
+See PEP 440 (https://www.python.org/dev/peps/pep-0440/)
 """
 __version__ = "0.7"
