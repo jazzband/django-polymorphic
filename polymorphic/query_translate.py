jazzband/django-polymorphic

commit 24e6b212046de58b39ab77b5c9eb1ae73e31ae2b

Fix a handful of warnings and remove some unused compatibility code

polymorphic/query_translate.py
@@ -162,7 +162,7 @@ def translate_polymorphic_field_path(queryset_model, field_path):
         # so no tripple ClassName___field was intended.
         try:
             # rel = (field_object, model, direct, m2m)
-            field = queryset_model._meta.get_field_by_name(classname)[0]
+            field = queryset_model._meta.get_field(classname)
             if isinstance(field, RelatedObject):
                 # Can also test whether the field exists in the related object to avoid ambiguity between
                 # class names and field names, but that never happens when your class names are in CamelCase.

polymorphic/tests.py
@@ -8,7 +8,7 @@ import re
 import django
 try:
     from unittest import skipIf
-except:
+except ImportError:
     # python<2.7
     from django.utils.unittest import skipIf
 from django.db.models.query import QuerySet
@@ -23,7 +23,11 @@ from polymorphic.models import PolymorphicModel
 from polymorphic.manager import PolymorphicManager
 from polymorphic.query import PolymorphicQuerySet
 from polymorphic import ShowFieldContent, ShowFieldType, ShowFieldTypeAndContent
-from polymorphic.tools_for_tests import UUIDField
+try:
+    from django.db.models import UUIDField
+except ImportError:
+    # django<1.8
+    from polymorphic.tools_for_tests import UUIDField
 
 
 class PlainA(models.Model):
@@ -212,15 +216,15 @@ class Bottom(Middle):
     author = models.CharField(max_length=50)
 
 class UUIDProject(ShowFieldTypeAndContent, PolymorphicModel):
-        uuid_primary_key = UUIDField(primary_key = True)
-        topic = models.CharField(max_length = 30)
+     uuid_primary_key = UUIDField(primary_key = True, default=uuid.uuid1)
+     topic = models.CharField(max_length = 30)
 class UUIDArtProject(UUIDProject):
-        artist = models.CharField(max_length = 30)
+     artist = models.CharField(max_length = 30)
 class UUIDResearchProject(UUIDProject):
-        supervisor = models.CharField(max_length = 30)
+     supervisor = models.CharField(max_length = 30)
 
 class UUIDPlainA(models.Model):
-    uuid_primary_key = UUIDField(primary_key = True)
+    uuid_primary_key = UUIDField(primary_key = True, default=uuid.uuid1)
     field1 = models.CharField(max_length=10)
 class UUIDPlainB(UUIDPlainA):
     field2 = models.CharField(max_length=10)

example/pexp/admin.py
@@ -42,36 +42,34 @@ class ModelAAdmin(PolymorphicParentModelAdmin):
 admin.site.register(ModelA, ModelAAdmin)
 
 
-if 'Model2A' in globals():
-    class Model2AChildAdmin(PolymorphicChildModelAdmin):
-        base_model = Model2A
+class Model2AChildAdmin(PolymorphicChildModelAdmin):
+    base_model = Model2A
 
-    class Model2AAdmin(PolymorphicParentModelAdmin):
-        base_model = Model2A
-        list_filter = (PolymorphicChildModelFilter,)
-        child_models = (
-            (Model2A, Model2AChildAdmin),
-            (Model2B, Model2AChildAdmin),
-            (Model2C, Model2AChildAdmin),
-        )
+class Model2AAdmin(PolymorphicParentModelAdmin):
+    base_model = Model2A
+    list_filter = (PolymorphicChildModelFilter,)
+    child_models = (
+        (Model2A, Model2AChildAdmin),
+        (Model2B, Model2AChildAdmin),
+        (Model2C, Model2AChildAdmin),
+    )
 
-    admin.site.register(Model2A, Model2AAdmin)
+admin.site.register(Model2A, Model2AAdmin)
 
 
-if 'UUIDModelA' in globals():
-    class UUIDModelAChildAdmin(PolymorphicChildModelAdmin):
-        base_model = UUIDModelA
+class UUIDModelAChildAdmin(PolymorphicChildModelAdmin):
+    base_model = UUIDModelA
 
-    class UUIDModelAAdmin(PolymorphicParentModelAdmin):
-        base_model = UUIDModelA
-        list_filter = (PolymorphicChildModelFilter,)
-        child_models = (
-            (UUIDModelA, UUIDModelAChildAdmin),
-            (UUIDModelB, UUIDModelAChildAdmin),
-            (UUIDModelC, UUIDModelAChildAdmin),
-        )
+class UUIDModelAAdmin(PolymorphicParentModelAdmin):
+    base_model = UUIDModelA
+    list_filter = (PolymorphicChildModelFilter,)
+    child_models = (
+        (UUIDModelA, UUIDModelAChildAdmin),
+        (UUIDModelB, UUIDModelAChildAdmin),
+        (UUIDModelC, UUIDModelAChildAdmin),
+    )
 
-    admin.site.register(UUIDModelA, UUIDModelAAdmin)
+admin.site.register(UUIDModelA, UUIDModelAAdmin)
 
 
 class ProxyChildAdmin(PolymorphicChildModelAdmin):

example/pexp/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import django
 from django.db import models
 
 from polymorphic.models import PolymorphicModel
@@ -26,27 +27,25 @@ class nModelB(nModelA):
 class nModelC(nModelB):
     field3 = models.CharField(max_length=10)
 
-# for Django 1.2+, test models with same names in different apps
-# (the other models with identical names are in polymorphic/tests.py)
-from django import VERSION as django_VERSION
-if not (django_VERSION[0]<=1 and django_VERSION[1]<=1):
-    class Model2A(PolymorphicModel):
-        field1 = models.CharField(max_length=10)
-    class Model2B(Model2A):
-        field2 = models.CharField(max_length=10)
-    class Model2C(Model2B):
-        field3 = models.CharField(max_length=10)
+class Model2A(PolymorphicModel):
+    field1 = models.CharField(max_length=10)
+class Model2B(Model2A):
+    field2 = models.CharField(max_length=10)
+class Model2C(Model2B):
+    field3 = models.CharField(max_length=10)
 
-try: from polymorphic.test_tools  import UUIDField
-except: pass
-if 'UUIDField' in globals():
-    class UUIDModelA(ShowFieldTypeAndContent, PolymorphicModel):
-        uuid_primary_key = UUIDField(primary_key = True)
-        field1 = models.CharField(max_length=10)
-    class UUIDModelB(UUIDModelA):
-        field2 = models.CharField(max_length=10)
-    class UUIDModelC(UUIDModelB):
-        field3 = models.CharField(max_length=10)
+if django.VERSION < (1,8):
+    from polymorphic.tools_for_tests import UUIDField
+else:
+    from django.db.models import UUIDField
+
+class UUIDModelA(ShowFieldTypeAndContent, PolymorphicModel):
+    uuid_primary_key = UUIDField(primary_key = True)
+    field1 = models.CharField(max_length=10)
+class UUIDModelB(UUIDModelA):
+    field2 = models.CharField(max_length=10)
+class UUIDModelC(UUIDModelB):
+    field3 = models.CharField(max_length=10)
 
 class ProxyBase(PolymorphicModel):
     title = models.CharField(max_length=200)

polymorphic/query.py
@@ -239,8 +239,8 @@ class PolymorphicQuerySet(QuerySet):
                 if real_class != real_concrete_class:
                     real_object = transmogrify(real_class, real_object)
 
-                if self.query.aggregates:
-                    for anno_field_name in six.iterkeys(self.query.aggregates):
+                if self.query.annotations:
+                    for anno_field_name in six.iterkeys(self.query.annotations):
                         attr = getattr(base_result_objects_by_id[o_pk], anno_field_name)
                         setattr(real_object, anno_field_name, attr)
 
@@ -255,8 +255,8 @@ class PolymorphicQuerySet(QuerySet):
         resultlist = [results[ordered_id] for ordered_id in ordered_id_list if ordered_id in results]
 
         # set polymorphic_annotate_names in all objects (currently just used for debugging/printing)
-        if self.query.aggregates:
-            annotate_names = list(six.iterkeys(self.query.aggregates))  # get annotate field list
+        if self.query.annotations:
+            annotate_names = list(six.iterkeys(self.query.annotations))  # get annotate field list
             for real_object in resultlist:
                 real_object.polymorphic_annotate_names = annotate_names
 

polymorphic/tools_for_tests.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
-####################################################################
-
+# Compatibility module for Django < 1.8
 import uuid
 
 from django import forms
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import six
+
 
 class UUIDVersionError(Exception):
     pass

commit 07242d210fd196893433d9a26d215c2c0e372d14

Fix "Model" __ "_field" query support
Broken by 24e6b212046de58b39ab77b5c9eb1ae73e31ae2b for Django 1.9
upgrade

polymorphic/query_translate.py
@@ -8,14 +8,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q, FieldDoesNotExist
 
-try:
-    from django.db.models.related import RelatedObject
-except ImportError:
-    # django.db.models.related.RelatedObject was replaced
-    # by django.db.models.fields.related.ForeignObjectRel in
-    # Django 1.8
-    from django.db.models.fields.related import ForeignObjectRel
-    RelatedObject = ForeignObjectRel
+from django.db.models.fields.related import RelatedField  # Django 1.8
 
 from functools import reduce
 
@@ -163,7 +156,7 @@ def translate_polymorphic_field_path(queryset_model, field_path):
         try:
             # rel = (field_object, model, direct, m2m)
             field = queryset_model._meta.get_field(classname)
-            if isinstance(field, RelatedObject):
+            if isinstance(field, RelatedField):
                 # Can also test whether the field exists in the related object to avoid ambiguity between
                 # class names and field names, but that never happens when your class names are in CamelCase.
                 return field_path  # No exception raised, field does exist.

polymorphic/tests.py
@@ -153,6 +153,15 @@ class One2OneRelatingModelDerived(One2OneRelatingModel):
     field2 = models.CharField(max_length=10)
 
 
+class ModelUnderRelParent(PolymorphicModel):
+    field1 = models.CharField(max_length=10)
+    _private = models.CharField(max_length=10)
+
+
+class ModelUnderRelChild(PolymorphicModel):
+    parent = models.ForeignKey(ModelUnderRelParent)
+
+
 class MyManagerQuerySet(PolymorphicQuerySet):
 
     def my_queryset_foo(self):
@@ -719,6 +728,14 @@ class PolymorphicTests(TestCase):
         self.assertEqual(len(objects), 2)
         self.assertEqual(repr(objects[0]), '<Model2B: id 2, field1 (CharField), field2 (CharField)>')
         self.assertEqual(repr(objects[1]), '<Model2C: id 3, field1 (CharField), field2 (CharField), field3 (CharField)>')
+
+    def test_polymorphic___filter_field(self):
+        p = ModelUnderRelParent.objects.create(_private=True, field1='AA')
+        ModelUnderRelChild.objects.create(parent=p)
+
+        # The "___" filter should also parse to "parent" -> "_private" as fallback.
+        objects = ModelUnderRelChild.objects.filter(parent___private=True)
+        self.assertEqual(len(objects), 1)
 
     def test_delete(self):
         self.create_model2abcd()