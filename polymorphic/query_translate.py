0ce882dd7bace728d19a40c9b6cdfafefc11df70
Diederik van der Boor <vdboor@edoburu.nl>
2016-06-06 15:26:48 +0200

    Make the Django 1.6 support more explicit for PR #218

--- polymorphic/query_translate.py
+++ polymorphic/query_translate.py
@@ -100,7 +100,10 @@ def translate_polymorphic_filter_definitions_in_args(queryset_model, args, using
 
     Returns: modified Q objects
     """
-    q_objects = [q if django.VERSION < (1, 6) else q.clone() for q in args]
+    if django.VERSION >= (1, 6):
+        q_objects = [q.clone() for q in args]
+    else:
+        q_objects = args  # NOTE: edits existing objects in place.
     return [translate_polymorphic_Q_object(queryset_model, q, using=using) for q in q_objects]
 
 
