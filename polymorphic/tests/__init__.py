d8cb700e5826b35594e5156e7a92d3e2c1e9eb28
Diederik van der Boor <vdboor@edoburu.nl>
2017-04-06 12:51:16 +0200

    Make sure tests can run in Django 1.11

--- polymorphic/tests/__init__.py
+++ polymorphic/tests/__init__.py
@@ -204,6 +204,7 @@ class MROBase2(MROBase1):
 
 
 class MROBase3(models.Model):
+    id2 = models.AutoField(primary_key=True)  # make sure 'id' field doesn't clash, detected by Django 1.11
     objects = PolymorphicManager()
 
 
