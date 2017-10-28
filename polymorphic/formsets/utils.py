120520e44d7dfcf3079bfdc9a118d28b5620cb14
Charlie Denton <charlie@meshy.co.uk>
2017-10-29 00:44:48 +0100

    Fix `add_media` util for Django 2.0
    Neither `add_css` nor `add_js` exist in Django 2.0 because the method
    for adding `Media` classes together has changed.
    
    Ref: https://github.com/django/django/commit/c19b56f633e172b3c02094cbe12d28865ee57772

--- polymorphic/formsets/utils.py
+++ polymorphic/formsets/utils.py
@@ -1,11 +1,17 @@
 """
 Internal utils
 """
+import django
 
 
 def add_media(dest, media):
     """
     Optimized version of django.forms.Media.__add__() that doesn't create new objects.
+
+    Only required for Django < 2.0
     """
-    dest.add_css(media._css)
-    dest.add_js(media._js)
+    if django.VERSION >= (2, 0):
+        dest += media
+    else:
+        dest.add_css(media._css)
+        dest.add_js(media._js)
