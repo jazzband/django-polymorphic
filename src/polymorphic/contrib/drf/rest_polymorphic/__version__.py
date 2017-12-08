ccee8684d02e8dba81165c3d8fea2621a8e83f18
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-12-08 16:05:02 +0300

    v0.1.1

--- src/polymorphic/contrib/drf/rest_polymorphic/__version__.py
+++ src/polymorphic/contrib/drf/rest_polymorphic/__version__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __title__ = 'django-rest-polymorphic'
 __description__ = 'Polymorphic serializers for Django REST Framework.'
 __url__ = 'https://github.com/apirobot/django-rest-polymorphic'
