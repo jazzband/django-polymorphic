0d102a173755c6d48ebac82de46dcf53b6d186be
jeff <jeff@mighty.com>
2019-05-24 11:43:18 -0400

    Fix add_media util for Django 2.2

--- polymorphic/formsets/utils.py
+++ polymorphic/formsets/utils.py
@@ -10,7 +10,10 @@ def add_media(dest, media):
 
     Only required for Django < 2.0
     """
-    if django.VERSION >= (2, 0):
+    if django.VERSION >= (2, 2):
+        dest._css_lists.extend(media._css_lists)
+        dest._js_lists.extend(media._js_lists)
+    elif django.VERSION >= (2, 0):
         combined = dest + media
         dest._css = combined._css
         dest._js = combined._js
