92406a484795278b3f6e1288d5e755fb17a4f872
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2019-10-02 21:23:10 +0600

    moved FiedDoesntExit import error

--- polymorphic/query.py
+++ polymorphic/query.py
@@ -7,8 +7,8 @@ from __future__ import absolute_import
 import copy
 from collections import defaultdict
 
+from django.core.exceptions import FieldDoesNotExist
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import FieldDoesNotExist
 from django.db.models.query import ModelIterable, Q, QuerySet
 
 from . import compat
