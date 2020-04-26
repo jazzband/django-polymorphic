9ce66d520346b20f0c4dd9fe7b809026e26a12b5
Denis Orehovsky <denis.orehovsky@gmail.com>
2020-04-26 15:52:03 +0300

    Update example project to new django version

--- example/example/wsgi.py
+++ example/example/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for example project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
 
 application = get_wsgi_application()
