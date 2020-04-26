db39109516e54cbffb02be1fcfa58ca2ebea780e
Denis Orehovsky <denis.orehovsky@gmail.com>
2020-04-26 15:52:03 +0300

    Update example project to new django version

--- src/polymorphic/contrib/drf/example/example/urls.py
+++ src/polymorphic/contrib/drf/example/example/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('projects.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls')),
 ]
