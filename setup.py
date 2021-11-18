e9341fe2fb1fef6b07edae192fd829b285e3d793
Diederik van der Boor <vdboor@edoburu.nl>
2021-11-18 12:55:03 +0100

    Fix compilemessages call for setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+# When creating the sdist, make sure the django.mo file also exists:
+if "sdist" in sys.argv or "develop" in sys.argv:
+    os.chdir("polymorphic")
+    try:
+        from django.core import management
+
+        management.call_command("compilemessages", stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if "sdist" in sys.argv:
+            raise
+    finally:
+        os.chdir("..")
 
 from setuptools import setup
 
