03e8e8dcd4f0bbca40c4206f83677c9821d4e2f8
Nick Merrill <1634334+nick-merrill@users.noreply.github.com>
2022-09-20 11:19:03 -0400

    Prevent version conflict error

--- polymorphic/__init__.py
+++ polymorphic/__init__.py
@@ -6,9 +6,5 @@ This code and affiliated files are (C) by Bert Constantin and individual contrib
 Please see LICENSE and AUTHORS for more information.
 """
 
-import pkg_resources
 
-try:
-    __version__ = pkg_resources.require("django-polymorphic")[0].version
-except pkg_resources.DistributionNotFound:
-    __version__ = None  # for RTD among others
+__version__ = "3.1.0"
